4f5b8c5c9e236e805b1eb53ec4e2f3e01eb61587
Karem Ceron <karemiceron@gmail.com>
2020-06-03 03:16:33 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,11 +1,4 @@
 def find_max_value(array)
-  
+  max_num = array[0]
+  array.size.times{ |i| if max_num < array[i] then max_num = array[0] }
 end
-
-
-numbers = [10, 20, 30, 40, 50, 60] # your array
-max_num = numbers[0] # Setting max_num to the first element of your array    
-numbers.size.times{|x| if max_num < numbers[x] then max_num = numbers[x] end} # I use 
- #* the .size method to find how many elements are on your array and the .times method
- #* to iterate "size" "times" over your array.  In the block you have a simple comparison
-puts max_num
