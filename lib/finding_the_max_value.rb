9c55138cbf88fa7f2bbce56e367f4a147eae455b
Karem Ceron <karemiceron@gmail.com>
2020-06-03 03:17:33 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,4 +1,5 @@
 def find_max_value(array)
   max_num = array[0]
-  array.size.times{ |i| if max_num < array[i] then max_num = array[0] }
+  array.size.times do |i| if max_num < array[i] then max_num = array[0] 
+end 
 end
